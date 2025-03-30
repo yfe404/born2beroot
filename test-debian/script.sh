60a32a16c02407c99886149b75f686b2bd1a70a8
yfe404 <yfe.github@protonmail.com>
2025-03-30 21:42:40 +0200

    wip

--- test-debian/script.sh
+++ test-debian/script.sh
@@ -4,28 +4,28 @@
 set -e # Exit on error
 
 DEVICE=$1
-[ -z "${DEVICE}" ] && echo "Usage $0 /dev/sdX" && exit 1
+[ -z "${DEVICE}" ] && echo "Usage: $0 /dev/sdX" && exit 1
 
 udevadm info -n ${DEVICE} -q property
 echo "Selected device is ${DEVICE}"
 read -p "[Press enter to continue or CTRL+C to stop]"
 
-echo "Umount ${DEVICE}"
+echo "Unmounting ${DEVICE}"
 umount ${DEVICE}* || true
 
-echo "Set partition table to MBR (BIOS)"
+echo "Setting partition table to MBR (BIOS)"
 dd if=/dev/zero of=${DEVICE} bs=1M count=1
 
 parted ${DEVICE} --script mklabel msdos
 parted ${DEVICE} --script mkpart primary 1MiB 512MiB
-parted ${DEVICE} --script set 1 bls_boot on
+parted ${DEVICE} --script set 1 boot on
 parted ${DEVICE} --script mkpart primary 512MiB 100%
 
-echo "Format partitions"
-mkfs.vfat  ${DEVICE}p1
-mkfs.ext4  -FL LINUX ${DEVICE}p2
+echo "Formatting partitions"
+mkfs.vfat ${DEVICE}p1
+mkfs.ext4 -FL LINUX ${DEVICE}p2
 
-echo "Mount OS partition"
+echo "Mounting OS partition"
 ROOTFS="/mnt/installing-rootfs"
 mkdir -p ${ROOTFS}
 mount ${DEVICE}p2 ${ROOTFS}
@@ -33,27 +33,47 @@ mount ${DEVICE}p2 ${ROOTFS}
 echo "Debootstrap system"
 debootstrap --variant=minbase --arch amd64 buster ${ROOTFS} http://deb.debian.org/debian/
 
-echo "Mount boot partition"
+echo "Mounting boot partition"
 mkdir -p ${ROOTFS}/boot
 mount ${DEVICE}p1 ${ROOTFS}/boot
 
-echo "Get ready for chroot"
+echo "Preparing chroot environment"
 mount --bind /dev ${ROOTFS}/dev
-mount -t devpts /dev/pts ${ROOTFS}/dev/pts
+mount -t devpts devpts ${ROOTFS}/dev/pts
 mount -t proc proc ${ROOTFS}/proc
 mount -t sysfs sysfs ${ROOTFS}/sys
 mount -t tmpfs tmpfs ${ROOTFS}/tmp
 
-echo "Entering chroot, installing Linux kernel and GRUB (BIOS mode)"
+echo "Entering chroot, installing Linux kernel and Limine"
 cat << EOF | chroot ${ROOTFS}
   set -e
   export HOME=/root
   export DEBIAN_FRONTEND=noninteractive
   export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin
+
   apt-get update
-  apt-get install -y linux-image-amd64 linux-headers-amd64 grub-pc
-  grub-install --target=i386-pc --boot-directory=/boot ${DEVICE}
-  update-grub
+  apt-get install -y linux-image-amd64 linux-headers-amd64 git make curl autoconf gcc nasm
+  git clone https://github.com/limine-bootloader/limine.git
+  cd limine
+  ./bootstrap
+  ./configue
+  make
+  make install
+
+  ROOT_UUID=\$(blkid -s UUID -o value ${DEVICE}p2)
+
+  echo "Creating Limine configuration file"
+  cat << LEOF > /boot/limine.cfg
+TIMEOUT=3
+:Linux
+PROTOCOL=linux
+KERNEL_PATH=/vmlinuz
+INITRD_PATH=/initrd.img
+CMDLINE=root=UUID=\$ROOT_UUID quiet
+LEOF
+
+  echo "Installing Limine"
+  limine-install ${DEVICE}
 EOF
 
 echo "Unmounting filesystems"
@@ -65,5 +85,4 @@ umount ${ROOTFS}/tmp
 umount ${ROOTFS}/boot
 umount ${ROOTFS}
 
-echo "Done"
-
+echo "Done! Reboot to test."
