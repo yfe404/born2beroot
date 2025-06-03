d5f149f8f56384eddb19bf4528f5d9674778c755
yfe404 <yfe.github@protonmail.com>
2025-06-03 05:15:59 +0200

    fix: command not found in cron env

--- monitoring.sh
+++ monitoring.sh
@@ -21,7 +21,7 @@ mem_perc=$(echo "scale=2; 100*$mem_use / $mem_avai" | bc)
 cpu_usage=$(top -bn 2 -d 0.1 | grep '^%Cpu' | tail -n 1 | awk '{print $2+$4+$6}')
 disk_usage=$(df -h --total | grep mapper | awk '{printf "\t-%s: Available: %s Usage: %s\n", $1, $4, $5}')
 lvm_in_use="no"
-if pvdisplay 2>/dev/null | grep -q UUID; then
+if /usr/sbin/pvdisplay 2>/dev/null | grep -q UUID; then
     lvm_in_use="yes"
 fi
 num_tcp_conn=$(ss -tn state established | tail -n +2 | wc -l)
